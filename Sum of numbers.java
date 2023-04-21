20899b7f3fd113d3058f95ddca0917f7869868ca
meilluer <114286084+meilluer@users.noreply.github.com>
2023-04-21 16:21:00 +0530

    Create Sum of numbers.java

---
+++ Sum of numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class add{
+    public static void main(String[] args){
+        int a=12;
+        int b=4;
+        int sum = a+b;
+        System.out.println("Sum of numbers is : "+sum);
+    }
+}
